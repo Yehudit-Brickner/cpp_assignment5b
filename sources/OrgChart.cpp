a8f006de9059635d2e8cc97d2443d9672c31aba7
Yehudit-Brickner <nech.diti@yahoo.com>
2022-05-23 00:28:37 +0300

    cpp

---
+++ sources/OrgChart.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include "OrgChart.hpp"
+using namespace std;
+using namespace ariel;
+
+OrgChart OrgChart::add_root(string s){
+    if( s.empty()){
+        throw std::invalid_argument( "empty string" );    
+    }
+    if (this->_root==nullptr){
+        this->_root=new Node(s);
+    }
+    else{
+        this->_root->data=s;
+    }
+    return *this;      
+}
+
+
+OrgChart OrgChart::add_sub(string s1,string s2){
+    if( s1.empty() || s2.empty()){
+            throw std::invalid_argument( "empty string" );    
+    }
+    if (this->_root==nullptr){
+        throw std::invalid_argument( "the orgchart is empty" ); 
+    }
+    queue<Node*> q;
+    queue<Node*>helper;
+    helper.push(this->_root);
+    while(helper.size()!=0){
+        if (helper.front()->data==s1){
+            helper.front()->kids.push_back(new Node (s2, helper.front()));
+            return *this;
+        }
+        q.push(helper.front());
+        getchildren(helper.front(), &helper); 
+        helper.pop();
+    }
+    // throw error parent no in orgchart 
+    throw std::invalid_argument( "parent is not in the orgchart" );   
+}
+
+
+void OrgChart::getchildren(Node* n, queue<Node*>* que){
+    for (unsigned long i =0; i< n->kids.size();i++){
+        que->push(n->kids[i]);
+    } 
+}
+
+
+ostream& ariel::operator<<(ostream& output, const OrgChart& org){
+    cout<< "printing OrgChart"<<endl; 
+    string s1="------------------";
+    string s2="---------";
+    string s3="|";
+    return output;
+}
+
+
+void OrgChart:: set_next__level_order(){
+    queue<Node*>q;
+    queue<Node*>helper;
+    helper.push(this->_root);
+    while(!helper.empty()){
+        q.push(helper.front());
+        getchildren(helper.front(), &helper); 
+        helper.pop();
+        if(!helper.empty()){
+            q.front()->next = helper.front();
+            q.pop();                   
+        }
+        else{
+            q.front()->next=nullptr;
+            q.pop();  
+        }
+    }
+}
+
+
+
+// Node* OrgChart::set_next__reverse_level_order(){
+//     queue<Node*>helper;
+//     stack<Node*>st;
+//     helper.push(this->_root);
+//     while(!helper.empty()){
+//         st.push(helper.front());
+//         getkids_q(st.top(), &helper); 
+//         helper.pop();
+//     }
+//     Node * beginig_of_iter=st.top();
+//     while(!st.empty()){
+//         helper.push(st.top());
+//         st.pop();
+//         if(!st.empty()){
+//             helper.front()->next=st.top();
+//             helper.pop();
+//         }
+//         else{
+//             helper.front()->next=nullptr;
+//             helper.pop();
+//         }
+//     }
+//     return beginig_of_iter; 
+// }
+
+
+
+
+void OrgChart::set_next__preorder(){
+    stack<Node*> helper;
+    queue<Node*> q;
+    helper.push(this->_root);
+    while(!helper.empty()){
+        q.push(helper.top());
+        helper.pop();
+        getkids_st(q.front(), &helper);
+        if(!helper.empty()){
+            q.front()->next = helper.top();
+            q.pop();                   
+        }
+        else{
+            q.front()->next = nullptr;
+            q.pop();
+        }
+    }    
+}
+
+
+void OrgChart::getkids_q(Node* n, queue<Node*>* que){
+    for (int i =n->kids.size()-1; i>=0;i--){
+        que->push(n->kids[(size_t)i]);
+    } 
+} 
+
+
+void OrgChart::getkids_st(Node* n, stack<Node*>* st){
+    for (int i =n->kids.size()-1; i>=0;i--){
+        st->push(n->kids[(size_t)i]);
+    } 
+}
--- sources/OrgChart.hpp
+++ sources/OrgChart.hpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <stdexcept>
 #include <unordered_map>
+#include <string>
 #include <vector>
 #include <queue>
 #include <stack>
@@ -22,97 +23,98 @@ namespace ariel{
                 Node* next;
 
                 Node(string &d) : data(d), parent(nullptr), next(nullptr){}
-
                 Node (string &d, Node * n) : data(d), parent(n), next(nullptr){}
-
                 ~Node(){
                     for(Node *n:kids){
                         delete(n);
                     }
                 }
             };
-
+            
             Node* _root;
 
             //constructer
             OrgChart():_root(nullptr){}
-            // ~OrgChart(){
-            //     queue<Node*>helper;
-            //     stack<Node*>st;
-            //     helper.push(this->_root);
-            //     while(!helper.empty()){
-            //         st.push(helper.front());
-            //         getkids_q(st.top(), &helper); 
-            //         helper.pop();
-            //     }
-            //     while(!st.empty()){
-            //         delete(st.top());
-            //         st.pop();
-            //     }
-                
-            // }
+            ~OrgChart(){
+                // queue<Node*>helper;
+                // stack<Node*>st;
+                // helper.push(this->_root);
+                // while(!helper.empty()){
+                //     st.push(helper.front());
+                //     getkids_q(st.top(), &helper); 
+                //     helper.pop();
+                // }
+                // while(!st.empty()){
+                //     delete(st.top());
+                //     st.pop();
+                // } 
+            }
             
             //create a "real root for the tree or change the string of the root
-            OrgChart add_root(string s){
-                if( s.empty()){
-                     throw std::invalid_argument( "empty string" );    
-                }
-                if (this->_root==nullptr){
-                    this->_root=new Node(s);
-                }
-                else{
-                    this->_root->data=s;
-                }
-                return *this;
-            }
+            OrgChart add_root(string s);
+            //     if( s.empty()){
+            //          throw std::invalid_argument( "empty string" );    
+            //     }
+            //     if (this->_root==nullptr){
+            //         this->_root=new Node(s);
+            //     }
+            //     else{
+            //         this->_root->data=s;
+            //     }
+            //     return *this;
+            // }
 
 
             // add a kid to one of the nodes in the tree
             // we will find the parent node and add the kid to its vector
-            OrgChart add_sub(string s1,string s2){
-                if( s1.empty() || s2.empty()){
-                     throw std::invalid_argument( "empty string" );    
-                }
-                if (this->_root==nullptr){
-                    throw std::invalid_argument( "the orgchart is empty" ); 
-                }
-                queue<Node*> q;
-                queue<Node*>helper;
-                helper.push(this->_root);
-                while(helper.size()!=0){
-                    if (helper.front()->data==s1){
-                        helper.front()->kids.push_back(new Node (s2, helper.front()));
-                        return *this;
-                    }
-                    q.push(helper.front());
-                    getchildren(helper.front(), &helper); 
-                    helper.pop();
-                }
-                // throw error parent no in orgchart 
-                throw std::invalid_argument( "parent is not in the orgchart" );   
-            }
+            OrgChart add_sub(string s1,string s2);
+               
+            //    {  if( s1.empty() || s2.empty()){
+                //      throw std::invalid_argument( "empty string" );    
+                // }
+                // if (this->_root==nullptr){
+                //     throw std::invalid_argument( "the orgchart is empty" ); 
+                // }
+                // queue<Node*> q;
+                // queue<Node*>helper;
+                // helper.push(this->_root);
+                // while(helper.size()!=0){
+                //     if (helper.front()->data==s1){
+                //         helper.front()->kids.push_back(new Node (s2, helper.front()));
+                //         return *this;
+                //     }
+                //     q.push(helper.front());
+                //     getchildren(helper.front(), &helper); 
+                //     helper.pop();
+                // }
+                // // throw error parent no in orgchart 
+                // throw std::invalid_argument( "parent is not in the orgchart" );   
+            //}
 
             // gets the kid of a node from right to left
-            void getchildren(Node* n, queue<Node*>* que){
-                for (unsigned long i =0; i< n->kids.size();i++){
-                    que->push(n->kids[i]);
-                } 
-            }
+            void getchildren(Node* n, queue<Node*>* que);
+            // {
+            //     for (unsigned long i =0; i< n->kids.size();i++){
+            //         que->push(n->kids[i]);
+            //     } 
+            // }
 
 
             // print the tree in a nice way
-            friend ostream& operator<<(ostream& output, const OrgChart& org){
-                cout<< "printing OrgChart"<<endl; 
-                string s1="------------------";
-                string s2="---------";
-                string s3="|";
-                return output;
-            }
+            friend ostream& operator<<(ostream& output, const OrgChart& org);
+            // {
+            //     cout<< "printing OrgChart"<<endl; 
+            //     string s1="------------------";
+            //     string s2="---------";
+            //     string s3="|";
+            //     return output;
+            // }
             
 
             class Iterator{
                 private:
                 Node* pointer_to_current_node;
+                
                 public:
                 Iterator(Node * n){
                     pointer_to_current_node=n;   
@@ -136,7 +138,6 @@ namespace ariel{
                     return iterator; 
                 }
 
-
                 bool operator==(const Iterator& other) const{
                     return pointer_to_current_node == other.pointer_to_current_node;
                 }
@@ -147,27 +148,29 @@ namespace ariel{
 
             } ;
 
-            void set_next__level_order(){
-                queue<Node*>q;
-                queue<Node*>helper;
-                helper.push(this->_root);
-                while(!helper.empty()){
-                    q.push(helper.front());
-                    getchildren(helper.front(), &helper); 
-                    helper.pop();
-                    if(!helper.empty()){
-                        q.front()->next = helper.front();
-                        q.pop();                   
-                    }
-                    else{
-                       q.front()->next=nullptr;
-                       q.pop();  
-                    }
-                }
-            }
+            void set_next__level_order();
+            // {
+            //     queue<Node*>q;
+            //     queue<Node*>helper;
+            //     helper.push(this->_root);
+            //     while(!helper.empty()){
+            //         q.push(helper.front());
+            //         getchildren(helper.front(), &helper); 
+            //         helper.pop();
+            //         if(!helper.empty()){
+            //             q.front()->next = helper.front();
+            //             q.pop();                   
+            //         }
+            //         else{
+            //            q.front()->next=nullptr;
+            //            q.pop();  
+            //         }
+            //     }
+            // }
 
 
-            Node* set_next__reverse_level_order(){
+            Node* set_next__reverse_level_order()
+            {
                 queue<Node*>helper;
                 stack<Node*>st;
                 helper.push(this->_root);
@@ -192,96 +195,99 @@ namespace ariel{
                 return beginig_of_iter;
             }
 
-            void set_next__preorder(){
-                stack<Node*> helper;
-                queue<Node*> q;
-                helper.push(this->_root);
-                while(!helper.empty()){
-                    q.push(helper.top());
-                    helper.pop();
-                    getkids_st(q.front(), &helper);
-                    if(!helper.empty()){
-                        q.front()->next = helper.top();
-                        q.pop();                   
-                    }
-                    else{
-                        q.front()->next = nullptr;
-                        q.pop();
-                    }
-                }    
-            }
+            void set_next__preorder();
+            // {
+            //     stack<Node*> helper;
+            //     queue<Node*> q;
+            //     helper.push(this->_root);
+            //     while(!helper.empty()){
+            //         q.push(helper.top());
+            //         helper.pop();
+            //         getkids_st(q.front(), &helper);
+            //         if(!helper.empty()){
+            //             q.front()->next = helper.top();
+            //             q.pop();                   
+            //         }
+            //         else{
+            //             q.front()->next = nullptr;
+            //             q.pop();
+            //         }
+            //     }    
+            // }
 
                   
-        void getkids_q(Node* n, queue<Node*>* que){
-            for (int i =n->kids.size()-1; i>=0;i--){
-                que->push(n->kids[(size_t)i]);
-            } 
-        } 
+        void getkids_q(Node* n, queue<Node*>* que);
+        // {
+        //     for (int i =n->kids.size()-1; i>=0;i--){
+        //         que->push(n->kids[(size_t)i]);
+        //     } 
+        // } 
 
 
-            void getkids_st(Node* n, stack<Node*>* st){
-                for (int i =n->kids.size()-1; i>=0;i--){
-                    st->push(n->kids[(size_t)i]);
-                } 
+        void getkids_st(Node* n, stack<Node*>* st);
+        // {
+        //     for (int i =n->kids.size()-1; i>=0;i--){
+        //         st->push(n->kids[(size_t)i]);
+        //     } 
+        // }
+
+
+
+
+        Iterator begin_level_order(){
+            if(this->_root==nullptr){
+                throw std::invalid_argument( "chart is empty!" );  
             }
+            set_next__level_order();
+            return Iterator(this->_root);
+        }
 
-
-
-
-            Iterator begin_level_order(){
+        Iterator end_level_order(){
                 if(this->_root==nullptr){
-                    throw std::invalid_argument( "chart is empty!" );  
-                }
-                set_next__level_order();
-                return Iterator(this->_root);
+                throw std::invalid_argument( "chart is empty!" );  
             }
+            return Iterator(nullptr);
+        }
 
-            Iterator end_level_order(){
-                 if(this->_root==nullptr){
-                    throw std::invalid_argument( "chart is empty!" );  
-                }
-                return Iterator(nullptr);
+        Iterator begin_reverse_order(){
+            if(this->_root==nullptr){
+                throw std::invalid_argument( "chart is empty!" );  
             }
-
-            Iterator begin_reverse_order(){
+            return Iterator(set_next__reverse_level_order());
+        }
+        Iterator reverse_order(){
                 if(this->_root==nullptr){
-                    throw std::invalid_argument( "chart is empty!" );  
-                }
-                return Iterator(set_next__reverse_level_order());
+                throw std::invalid_argument( "chart is empty!" );  
             }
-            Iterator reverse_order(){
-                 if(this->_root==nullptr){
-                    throw std::invalid_argument( "chart is empty!" );  
-                }
-                return Iterator(nullptr); 
-            }
+            return Iterator(nullptr); 
+        }
 
-            Iterator begin_preorder(){
+        Iterator begin_preorder(){
+            if(this->_root==nullptr){
+                throw std::invalid_argument( "chart is empty!" );  
+            }
+            set_next__preorder();
+            return Iterator(this->_root);
+        }
+        Iterator end_preorder(){
                 if(this->_root==nullptr){
-                    throw std::invalid_argument( "chart is empty!" );  
-                }
-                set_next__preorder();
-                return Iterator(this->_root);
+                throw std::invalid_argument( "chart is empty!" );  
             }
-            Iterator end_preorder(){
-                 if(this->_root==nullptr){
-                    throw std::invalid_argument( "chart is empty!" );  
-                }
-                return Iterator(nullptr);
-            }
+            return Iterator(nullptr);
+        }
 
-            Iterator begin(){
-                 if(this->_root==nullptr){
-                    throw std::invalid_argument( "chart is empty!" );  
-                }
-                return begin_level_order();
+        Iterator begin(){
+                if(this->_root==nullptr){
+                throw std::invalid_argument( "chart is empty!" );  
             }
-            Iterator end(){
-                 if(this->_root==nullptr){
-                    throw std::invalid_argument( "chart is empty!" );  
-                }
-                return end_level_order();
+            return begin_level_order();
+        }
+        Iterator end(){
+                if(this->_root==nullptr){
+                throw std::invalid_argument( "chart is empty!" );  
             }
+            return end_level_order();
+        }
         
 
     };
